4378b288749721f1cb49245dedd165f80155c5b3
eager7 <panchagntao@gmail.com>
2017-01-13 18:37:02 +0800

    将灯应用加入工程

---
+++ profiles/light_bulb.c
@@ -0,0 +1,94 @@
+/****************************************************************************
+ *
+ * MODULE:             light_bulb.c
+ *
+ * COMPONENT:          home kit interface
+ *
+ * REVISION:           $Revision:  1.0$
+ *
+ * DATED:              $Date: 2017-01-11 15:13:17 +0100 (Fri, 12 Dec 2016 $
+ *
+ * AUTHOR:             PCT
+ *
+ ****************************************************************************
+ *
+ * Copyright panchangtao@gmail.com 2017. All rights reserved
+ *
+ ***************************************************************************/
+
+/****************************************************************************/
+/***        Include files                                                 ***/
+/****************************************************************************/
+#include "light_bulb.h"
+#include "profile.h"
+
+/****************************************************************************/
+/***        Macro Definitions                                             ***/
+/****************************************************************************/
+#define DBG_PROFILE (1)
+/****************************************************************************/
+/***        Type Definitions                                              ***/
+/****************************************************************************/
+
+/****************************************************************************/
+/***        Local Function Prototypes                                     ***/
+/****************************************************************************/
+static teProfileStatus eLightBulbOn(tsAccessory *psAccessory);
+static teProfileStatus eLightBulbOff(tsAccessory *psAccessory);
+static teProfileStatus eLightBulbHandle(teProfileCmd eProfileCmd);
+/****************************************************************************/
+/***        Exported Variables                                            ***/
+/****************************************************************************/
+tsProfile sLightBulb;
+
+tsProfileHandle sLightBulbHandle[] = {
+        {E_PROFILE_CMD_LIGHT_BULB_ON, eLightBulbOn},
+        {E_PROFILE_CMD_LIGHT_BULB_OFF, eLightBulbOff},
+};
+
+
+
+/****************************************************************************/
+/***        Local Variables                                               ***/
+/****************************************************************************/
+
+/****************************************************************************/
+/***        Exported Functions                                            ***/
+/****************************************************************************/
+teProfileStatus eLightBulbProfileInit(char *psName, char *psSerialNumber, char *psManufacturer, char *psModel)
+{
+    memset(&sLightBulb, 0, sizeof(sLightBulb));
+    eAccessoryInit(&sLightBulb.sAccessory, psName, psSerialNumber, psManufacturer, psModel, E_HAP_TYPE_LIGHT_BULB);
+    sLightBulb.peCallbackFunc = eLightBulbHandle;
+
+    return E_PROFILE_OK;
+}
+
+teProfileStatus eLightBulbProfileFinished(void)
+{
+    eAccessoryFinished(&sLightBulb.sAccessory);
+    return E_PROFILE_OK;
+}
+/****************************************************************************/
+/***        Local    Functions                                            ***/
+/****************************************************************************/
+static teProfileStatus eLightBulbHandle(teProfileCmd eProfileCmd)
+{
+    for(int i = 0; i < sizeof(sLightBulbHandle)/sizeof(tsProfileHandle); i++){
+        if((sLightBulbHandle[i].eProfileCmd == eProfileCmd)&&(sLightBulbHandle[i].peHandleFunc != NULL)){
+            return sLightBulbHandle[i].peHandleFunc(&sLightBulb.sAccessory);
+        }
+    }
+    return E_PROFILE_ERROR;
+}
+
+static teProfileStatus eLightBulbOn(tsAccessory *psAccessory)
+{
+    DBG_vPrintln(DBG_PROFILE, "eLightBulbOn");
+    return E_PROFILE_OK;
+}
+static teProfileStatus eLightBulbOff(tsAccessory *psAccessory)
+{
+    DBG_vPrintln(DBG_PROFILE, "eLightBulbOff");
+    return E_PROFILE_OK;
+}
---
+++ profiles/profile.h
@@ -0,0 +1,78 @@
+/****************************************************************************
+ *
+ * MODULE:             profile.h
+ *
+ * COMPONENT:          home kit interface
+ *
+ * REVISION:           $Revision:  1.0$
+ *
+ * DATED:              $Date: 2017-01-11 15:13:17 +0100 (Fri, 12 Dec 2016 $
+ *
+ * AUTHOR:             PCT
+ *
+ ****************************************************************************
+ *
+ * Copyright panchangtao@gmail.com 2017. All rights reserved
+ *
+ ***************************************************************************/
+
+#ifndef HOMEKIT_PROFILE_H
+#define HOMEKIT_PROFILE_H
+#if defined __cplusplus
+extern "C" {
+#endif
+/****************************************************************************/
+/***        Include files                                                 ***/
+/****************************************************************************/
+#include "accessory.h"
+
+/****************************************************************************/
+/***        Macro Definitions                                             ***/
+/****************************************************************************/
+
+/****************************************************************************/
+/***        Type Definitions                                              ***/
+/****************************************************************************/
+typedef enum {
+    E_PROFILE_OK,
+    E_PROFILE_ERROR,
+} teProfileStatus;
+
+typedef enum {
+    E_PROFILE_CMD_LIGHT_BULB_ON,
+    E_PROFILE_CMD_LIGHT_BULB_OFF,
+} teProfileCmd;
+
+typedef teProfileStatus (*fpeHandleFunc)(tsAccessory *psAccessory);
+typedef teProfileStatus (*fpeCallbackFunc)(teProfileCmd eProfileCmd);
+
+typedef struct {
+    teProfileCmd eProfileCmd;
+    fpeHandleFunc peHandleFunc;
+} tsProfileHandle;
+
+typedef struct {
+    tsAccessory     sAccessory;
+    fpeCallbackFunc peCallbackFunc;
+} tsProfile;
+/****************************************************************************/
+/***        Local Function Prototypes                                     ***/
+/****************************************************************************/
+
+/****************************************************************************/
+/***        Exported Variables                                            ***/
+/****************************************************************************/
+/****************************************************************************/
+/***        Local Variables                                               ***/
+/****************************************************************************/
+
+/****************************************************************************/
+/***        Exported Functions                                            ***/
+/****************************************************************************/
+/****************************************************************************/
+/***        Local    Functions                                            ***/
+/****************************************************************************/
+#if defined __cplusplus
+}
+#endif
+#endif //HOMEKIT_PROFILE_H
