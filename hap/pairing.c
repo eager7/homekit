b4bbd799ad04e51791399117476451a3f3aa3497
eager7 <panchagntao@gmail.com>
2017-02-23 18:07:37 +0800

    将乘法改成移位

--- hap/pairing.c
+++ hap/pairing.c
@@ -536,8 +536,8 @@ teHapStatus eHandleAccessoryRequest(int iSocketFd, tsBonjour *psBonjour)
             WAR_vPrintln(DBG_PAIR, "Disconnect Socket");
             return E_HAP_STATUS_ERROR;
         }
-        //uint16 u16MsgLen = (sController.auBuffer[0] | ((uint16)sController.auBuffer[1] << 0xff));
-        uint16 u16MsgLen = (uint16)((uint8)sController.auBuffer[1]*256 + (uint8)*sController.auBuffer);
+        uint16 u16MsgLen = (sController.auBuffer[0] | ((uint16)sController.auBuffer[1] << 0xff));
+        //uint16 u16MsgLen = (uint16)((uint8)sController.auBuffer[1]*256 + (uint8)*sController.auBuffer);
 
         chacha20_ctx chacha20;
         memset(&chacha20, 0, sizeof(chacha20));
